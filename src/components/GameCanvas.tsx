2e7e505a6f09667c444c79b468ba7621f9730dea
Dorian <98755570+doriangirard9@users.noreply.github.com>
2025-02-22 18:35:23 +0100

    styles: player crosshair

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { Game } from '../lib/game';
+import '../styles/gameCanvas.css';
 
 const GameCanvas = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -13,11 +14,15 @@ const GameCanvas = () => {
   }, [canvasRef]);
 
   return (
-    <canvas
-      ref={canvasRef}
-      height={window.innerHeight}
-      width={window.innerWidth}
-    ></canvas>
+    <>
+      <div className="playerCrosshair verticalLine"></div>
+      <div className="playerCrosshair horizontalLine"></div>
+      <canvas
+        ref={canvasRef}
+        height={window.innerHeight}
+        width={window.innerWidth}
+      ></canvas>
+    </>
   );
 };
 
